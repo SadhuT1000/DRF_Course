a3a8991e9a65c1dffb82a065dbe906e9a3723c51
maksbolomoznov <bolomozhnov@gmail.com>
2025-01-19 18:40:22 +0300

    119

--- habits/tasks.py
+++ habits/tasks.py
@@ -1,13 +1,14 @@
-# flake8: noqa
-
 from celery import shared_task
+from datetime import datetime, timedelta
 
 import users
 from habits.models import Habit
 from habits.services import send_telegram
 
-from .models import Habit
 
+
+from datetime import datetime, timedelta
+from .models import Habit
 
 
 
@@ -22,12 +23,19 @@ def send_reminder():
     now = datetime.now()
     time_threshold = now - timedelta(seconds=40)
 
-    # Фильтруем привычки, которые нужно напомнить
+
     habits_to_remind = Habit.objects.filter(time_for_habit__lte=now.time(), time_for_habit__gte=time_threshold.time())
 
     for habit in habits_to_remind:
-        chat_id = habit.user.tg_chat_id
+        chat_id = users.tg_id
         if chat_id:
             message = f'Я буду {habit.action} в {habit.time_for_habit} в {habit.place}'
             send_telegram(chat_id, message)
             print('Сообщение отправлено')
+
+
+
+
+
+
+
